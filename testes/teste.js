b1e44d255fb3bddbea3c3e91c28ccf51a966d094
Luciano Andrade <lunodrade@gmail.com>
2019-07-11 16:33:17 -0300

    Self configure

---
+++ testes/teste.js
@@ -0,0 +1,29 @@
+//esse arquivo tem varias linhas em branco
+
+
+
+
+
+function        essa_funcao_comeca_com_muitos_espacos         ( ne )
+
+//a abertura dela ta na outra linha
+{
+  //essa variavel ta sem var|let|const (precisa ser manualmente configurada)
+  xx = 10;
+  
+}
+
+              function tudo_certo_exceto_identacao(alo) {
+                var xx = alo;
+              }
+
+            
+const xx = 12;
+
+
+var tes = "ASPAS DUPLAS";
+const xx = 12;                      //repete a linha 20 (precisa manualmente)
+
+const a = 15;
+
+//o resto resolve com salvar
