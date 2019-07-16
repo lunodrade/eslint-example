7fe8a551658949137a03b602297112070f30fb38
Luciano Andrade <lunodrade@gmail.com>
2019-07-16 08:49:59 -0300

    Update teste.blade.php

--- testes/teste.blade.php
+++ testes/teste.blade.php
@@ -1,12 +1,8 @@
 @extends('adminlte::master') @section('adminlte_css')
-<link
-  rel="stylesheet"
-  href="{{ asset('vendor/adminlte/plugins/iCheck/square/blue.css') }}"
-/>
+<link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/iCheck/square/blue.css') }}" />
 <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/auth.css') }}" />
 @yield('css') @stop @section('body_class', 'login-page') @section('body')
-<div class="login-box">
-  <div class="login-logo">
+<div class="login-box">  <div class="login-logo">
     <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}"
       >{!! config('adminlte.logo', '<b>M</b>igra') !!}</a
     >
@@ -14,32 +10,18 @@
   <!-- /.login-logo -->
   <div class="login-box-body">
     <p class="login-box-msg">{{ trans('adminlte::adminlte.login_message') }}</p>
-    <forms
-      action="{{ url(config('adminlte.login_url', 'login')) }}"
-      method="post"
-    >
+    <forms action="{{ url(config('adminlte.login_url', 'login')) }}" method="post" >
       {!! csrf_field() !!}
-
-      <div
-        class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}"
-      >
-        <input
-          type="email"
-          name="email"
-          class="form-control"
-          value="{{ old('email') }}"
-          placeholder="{{ trans('adminlte::adminlte.email') }}"
-        />
+      <div        class="form-group has-feedback {{ $errors->has('email') ? 'has-error' : '' }}"      >
+        <input type="email" name="email"
+          class="form-control" value="{{ old('email') }}"
+          placeholder="{{ trans('adminlte::adminlte.email') }}"        />
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-        @if ($errors->has('email'))
-        <span class="help-block">
-          <strong>{{ $errors->first('email') }}</strong>
-        </span>
-        @endif
-      </div>
-    </forms>
-    <!-- /.login-box-body -->
-  </div>
+@if ($errors->has('email'))
+<span class="help-block"><strong>{{ $errors->first('email') }}</strong>
+</span>
+@endif
+</div>    </forms>    <!-- /.login-box-body -->  </div>
   <!-- /.login-box -->
   @stop @section('adminlte_js')
   <script src="{{
@@ -47,12 +29,9 @@
     }}"></script>
   <script>
     $(function() {
-      $('input').iCheck({
-        checkboxClass: 'icheckbox_square-blue',
-        radioClass: 'iradio_square-blue',
-        increaseArea: '20%' // optional
-      });
+      $('input').iCheck({        checkboxClass: 'icheckbox_square-blue',        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional      });
     });
-  </script>
+              </script>
   @yield('js') @stop
 </div>
